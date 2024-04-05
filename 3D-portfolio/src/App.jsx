bb2593afa1143003468bc3582ad6a16bdbc3fa7a
Gautam Ankoji <gautamankoji@gmail.com>
2024-04-05 21:12:30 +0530

    100 web development projects

---
+++ 3D-portfolio/src/App.jsx
@@ -0,0 +1,28 @@
+// <!-- Developed By Gautam Ankoji -->
+
+import { BrowserRouter } from "react-router-dom";
+import { Hero, Navbar, StarsCanvas, About, Experience, Works, Feedbacks, Contact } from "./components";
+// import './App.css'; // Import your custom styles
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className='app-container'>
+        <div className='hero-background'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Works />
+        <Feedbacks />
+        <div className='stars-container'>
+          <StarsCanvas />
+          <Contact />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
